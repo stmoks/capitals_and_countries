f6e03fae4a31d4484b6c32fb236207eb37d22886
Sechaba Mokobane <sechaba.mokobane@gmail.com>
2024-07-03 19:49:08 +0200

    add downloaded svg flags

--- app/test.py
+++ app/test.py
@@ -1,18 +1,15 @@
-#%%
-import pandas as pd
-import polars as pl
+# #%%
+# import pandas as pd
+# import polars as pl
+
+# flags_url = 'https://en.wikipedia.org/wiki/Gallery_of_sovereign_state_flags'
 
 
-
-#%%
-news_url = 'https://en.wikipedia.org/wiki/Portal:Current_events'
-list_of_tables = pd.read_html(news_url)
-news_date_history = list_of_tables[3]
-reported_countries = list_of_tables[2].iloc[0,0]
+# #%%
+# # news_url = 'https://en.wikipedia.org/wiki/Portal:Current_events'
+# list_of_tables = pd.read_html(flags_url)
+# news_date_history = list_of_tables[3]
+# reported_countries = list_of_tables[2].iloc[0,0]
 # %%
 
 
-
-import folium
-m = folium.Map(location=[09.0580, 007.4891])
-folium.Map(location=[(09.0580, 007.4891),[-09.0580, -007.4891])])
