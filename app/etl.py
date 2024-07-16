62dc1d9d1c8472a23317967d6508711d7215d8ab
Sechaba Mokobane <sechaba.mokobane@gmail.com>
2024-07-16 05:42:11 +0200

    add missing flags, separate transformations from main

--- app/etl.py
+++ app/etl.py
@@ -7,6 +7,7 @@ import glob
 import re
 from PIL import Image
 import io
+from thefuzz import process,fuzz
 
 
 #%%
@@ -29,8 +30,6 @@ cities_df = list_of_tables[1]
 
 #%%
 capitals_df
-
-
 #%%
 # change column names
 capitals_df = pl.from_pandas(capitals_df,rechunk=True)
@@ -42,7 +41,6 @@ cities_df
 
 
 #%%
-# extract all the data before the extra information (brackets in this case)
 # remove special characters from the city and country names so we can join two datasets later
 capitals_df = capitals_df.with_columns(
     pl.col('city').str.replace(r'\s*\(.*', '').replace(r'[\',.]',''),
@@ -65,14 +63,18 @@ capitals_with_coordinates = capitals_df.join(cities_df[['city','country','latitu
 
 # add combined data to database, schema on write
 capitals_with_coordinates = capitals_with_coordinates.with_columns(pl.lit(None).alias('flag'))
-capitals_with_coordinates.write_database(table_name='country_info',connection=f'sqlite:///{db_name}',if_table_exists='replace')
+
 #%%
+#add sdc coordinates
+capitals_with_coordinates_sdc = capitals_with_coordinates.filter(pl.col('latitude').is_not_null()).with_columns(pl.col('longitude').str.replace('E','').str.replace('W','-').alias('longitude_sdc'),pl.col('latitude').str.replace('S','-').str.replace('N','').alias('latitude_sdc')).with_columns((pl.col('latitude_sdc') + ',' + pl.col('longitude_sdc') + ',' + pl.col('city')).alias('coords_city_sdc'))
+
+#write country data to the db
+capitals_with_coordinates_sdc.write_database(table_name='country_info',connection=f'sqlite:///{db_name}',if_table_exists='replace')
 
 #create flags table
 drop_table_sql = f'''DROP TABLE IF EXISTS flags;'''
 create_table_sql = f'''CREATE TABLE flags (country VARCHAR(200), flag BLOB);'''
 try:
-   
     db_cursor.execute(drop_table_sql)
     db_cursor.execute(create_table_sql)
     db_conn.commit()
@@ -81,7 +83,7 @@ except:
     print('Cannot create table, check that you don"t have any errors in your code')
 
 # add flags to database
-path = 'flags'
+path = 'app/flags'
 flags_list = []
 filenames = glob.glob(f'{path}/*.svg.png')
 flags_dict = {}
@@ -93,7 +95,7 @@ for file in filenames:
     try:
         with open(file,'rb') as f:
             # get rid of dots and square brackets, create country and flag pair
-            flags_dict = {'country':re.split('[/.\\[\\]]',file)[1],'flag': f.read()}
+            flags_dict = {'country':re.split('[/.\\[\\]]',file)[2],'flag': f.read()}
         insert_data_sql = '''INSERT INTO flags (country,flag) VALUES (?,?);'''
         db_cursor.execute(insert_data_sql,(flags_dict['country'],flags_dict['flag']))
         print(f'added {flags_dict['country']}')
@@ -101,8 +103,15 @@ for file in filenames:
         print(f'Could not read {file}')
 
 
-pd.read_sql('SELECT * FROM flags',db_conn)
+pl.read_database('SELECT * FROM flags',db_conn)
 
+#%%
+# create countries ref table, fuzzy match to fix issue with flags table
+flags_df = pl.read_database('SELECT country FROM flags',db_conn)
+countries_ref_df = capitals_df.select(['country'])
+
+#TODO create join on join table for flag names
+countries_ref_df.map_rows(lambda x: (process.extractOne(x[0],flags_df['country'],scorer=fuzz.token_set_ratio))[0] + ';' + x[0]).select(pl.col('map').str.split(';'))
 
 
 #%%
@@ -116,9 +125,10 @@ except:
     print('Had to rollback, could not update country_info table with flags')
 
 #%%
+pl.read_database('SELECT * FROM flags',db_conn)
 
-# create countries ref table, fuzzy match to fix issue with flags table
+pl.read_database('SELECT * FROM country_info where flag IS NOT NULL',db_conn)
 
+# %%
 db_conn.close()
-
 # %%
--- app/main.py
+++ app/main.py
@@ -7,24 +7,27 @@ import folium
 
 
 #%%
-conn = sqlite3.connect('capitals_db')
+# create database connection
+db_name = 'countries.db'
 
-country_info = 
-# get info about the new dataframe
-print(capitals_with_coordinates.glimpse())
+
+db_conn = sqlite3.connect(db_name)
+db_cursor = sqlite3.Cursor(db_conn)
+
+
 # %%
-
-capitals_with_coordinates_sdc = capitals_with_coordinates.filter(pl.col('latitude').is_not_null()).with_columns(pl.col('longitude').str.replace('E','').str.replace('W','-').alias('longitude_sdc'),pl.col('latitude').str.replace('S','-').str.replace('N','').alias('latitude_sdc')).with_columns((pl.col('latitude_sdc') + ',' + pl.col('longitude_sdc') + ',' + pl.col('city')).alias('coords_city_sdc'))
 
 m = folium.Map(location=[-29, 25], 
     zoom_start= 5)
 
+#TODO acknowledging the other connection, but not this
+country_info = pl.read_database('SELECT * FROM country_info where flag IS NOT NULL',db_conn)
 
 
-capitals_with_coordinates_sdc.with_columns(
+country_info.with_columns(
     pl.col('coords_city_sdc').map_elements(lambda x: folium.CircleMarker(location = x.split(',')[0:2], radius=15,
     fill=True,
-    popup=capitals_with_coordinates_sdc.filter(pl.col('city')== x.split(',')[2]).select('country')._repr_html_()).add_to(m)))
+    popup=country_info.filter(pl.col('city')== x.split(',')[2]).select('flag')._repr_html_()).add_to(m)))
 m
 
 
