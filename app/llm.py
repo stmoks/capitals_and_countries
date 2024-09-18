40f11164a42b48d0dc4a73e81fd8febc653feac8
sechaba.mokobane <sechaba.mokobane@gmail.com>
2024-09-18 06:21:21 +0200

    pull unordered lists from scrape

--- app/llm.py
+++ app/llm.py
@@ -4,6 +4,7 @@ import time
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import os
 
+#%%
 # Define function that prompts the model 
 def prompt_model(prompt, model_name, hf_access_token, max_length=1024, device=None):
    
@@ -16,15 +17,18 @@ def prompt_model(prompt, model_name, hf_access_token, max_length=1024, device=No
     tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", token=hf_access_token, trust_remote_code=True)
     model = AutoModelForCausalLM.from_pretrained(model_name, token=hf_access_token, trust_remote_code=True)
     model.to(device).eval()
+    print('yay')
 
     # Encode prompt
     inputs = tokenizer(prompt, return_tensors="pt", truncation=True, max_length=512).to(device)
+    print('nay')
 
     # Generate and time the process
     start_time = time.time()
     outputs = model.generate(inputs["input_ids"], max_length=max_length)
     generation_time = time.time() - start_time
-
+    print('yyyyyyyyy')
+    print(generation_time)
     # Decode and print output
     generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
     print(f"Generated in {generation_time:.2f} seconds: {generated_text}")
@@ -33,8 +37,12 @@ def prompt_model(prompt, model_name, hf_access_token, max_length=1024, device=No
 #%%
 model_name = "apple/OpenELM-3B-Instruct"
 # Give your Hugging Face Token
-hf_access_token = os.environ('hf_token')
+# hf_access_token = os.environ('hf_token')
+hf_access_token = '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'
 # Define Prompt
-prompt = "Name all the countries in the world, list of current vs historical"
+prompt = "\nName all the countries in the world, list of current vs historical"
+print(prompt)
+print(hf_access_token)
 # Prompt the model
 prompt_model(prompt, model_name, hf_access_token)
+# %%
--- app/news_scraping.py
+++ app/news_scraping.py
@@ -2,6 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import re
 
 # Define the URL of the Wikipedia page
 url = 'https://en.wikipedia.org/wiki/Portal:Current_events'
@@ -16,6 +17,9 @@ soup = BeautifulSoup(response.content, 'html.parser')
 
 #%%
 text = soup.getText()
+print(True) if 'South African' in text else print('Nope')
 
+news_items = soup.find_all('ul')
+news_items
 
 # %%
